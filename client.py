e43c68b0f2178b592a0231318dd02076ed59bc84
Samuel Dwomoh <sdwomoh418@gmail.com>
2026-06-04 01:26:02 +0000

    Top frame worked on

---
+++ client.py
@@ -0,0 +1,28 @@
+import tkinter as tk
+
+def connect():
+    pass
+
+def disconnect():
+    pass
+
+root = tk.Tk()
+root.title("Group 5")
+root.geometry("1280x720")
+
+top_frame = tk.Frame(root)
+top_frame.pack(pady=10)
+
+label = tk.Label(top_frame, text="Stream URL:")
+label.pack(side="left", padx=5)
+
+entry = tk.Entry(top_frame, width=50)
+entry.pack(side="left", padx=5)
+
+connect_button = tk.Button(top_frame, text="Connect", command=connect)
+connect_button.pack(side="left", padx=5)
+
+disconnect_button = tk.Button(top_frame, text="Disconnect", command=disconnect)
+disconnect_button.pack(side="left", padx=5)
+
+root.mainloop()
