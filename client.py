32e4ac7ae9635812f140bf3e263e3a937bfcb3f8
Samuel Dwomoh <sdwomoh418@gmail.com>
2026-06-04 01:31:52 +0000

    Divided parts of GUI into frames for easier comprehension.

--- client.py
+++ client.py
@@ -6,10 +6,13 @@ def connect():
 def disconnect():
     pass
 
+
+'''Main application windoww setup'''
 root = tk.Tk()
 root.title("Group 5")
 root.geometry("1280x720")
 
+'''Top frame for stream URL input and connect/disconnect buttons'''
 top_frame = tk.Frame(root)
 top_frame.pack(pady=10)
 
@@ -25,4 +28,6 @@ connect_button.pack(side="left", padx=5)
 disconnect_button = tk.Button(top_frame, text="Disconnect", command=disconnect)
 disconnect_button.pack(side="left", padx=5)
 
+'''Main frame for video display'''
+'''Control buttons frame for play, pause, stop, teardown'''
 root.mainloop()
